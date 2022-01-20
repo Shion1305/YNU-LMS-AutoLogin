029090cce30850fd216e2dc827f6858713546efd
Shion Ichikawa <shion1305@gmail.com>
2022-01-20 21:46:39 +0900

    Created login_script.js(which is internal file) Added .gitignore

--- login_script.js
+++ login_script.js
@@ -1,10 +1,13 @@
-var username='b2164027'
-var password=''
+var username = ''
+var password = ''
+
+myConfig();
 
 //Bypass LMS password by autofill and auto-submit
 document.getElementById("loginForm").userId.value = username;
 document.getElementById("loginForm").password.value = password;
 document.getElementById("loginForm").submit();
+
 async function getFromStorage() {
     return new Promise(resolve => {
         chrome.storage.local.get((data) => {
