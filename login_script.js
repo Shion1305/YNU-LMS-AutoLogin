38b77d2719c24ed9d9541093028dde639843749b
Shion Ichikawa <shion1305@gmail.com>
2022-04-10 12:43:39 +0900

    [IMPLEMENT] auto password save, auto removal on error function

--- login_script.js
+++ login_script.js
@@ -1,8 +1,29 @@
+let loginForm = document.getElementById("loginForm");
+getFromStorage();
 
 async function getFromStorage() {
     return new Promise(resolve => {
         chrome.storage.local.get((data) => {
-            resolve(data)
+            console.log(data.userData);
+            if (data.userData === undefined) {
+                loginForm.addEventListener("submit", async (event) => {
+                    let data = {
+                        userData: {
+                            username: loginForm.userId.value,
+                            password: loginForm.password.value
+                        }
+                    };
+                    chrome.storage.local.set(data);
+                });
+            } else {
+                if (document.getElementById("addMess2") === null) {
+                    loginForm.userId.value = data.userData.username;
+                    loginForm.password.value = data.userData.password;
+                    loginForm.submit();
+                } else {
+                    chrome.storage.local.clear();
+                }
+            }
         });
     });
 }
