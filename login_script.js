5ae864cc8d69326f833d0a15e6ecc1935ef8d4a1
Shion Ichikawa <shion1305@gmail.com>
2022-04-10 12:43:01 +0900

    [REMOVE] customscript.js, related to myConfig, which is an old way

--- login_script.js
+++ login_script.js
@@ -1,15 +1,3 @@
-var username = ''
-var password = ''
-
-
-// if myConfig() exists as function, execute.
-if (typeof myConfig() == 'function') {
-    myConfig();
-}
-//Bypass LMS password by autofill and auto-submit
-document.getElementById("loginForm").userId.value = username;
-document.getElementById("loginForm").password.value = password;
-document.getElementById("loginForm").submit();
 
 async function getFromStorage() {
     return new Promise(resolve => {
